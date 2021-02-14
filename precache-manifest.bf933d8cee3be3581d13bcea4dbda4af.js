b7d9365e53f10de25bcf3acaf33ef8149ed5057c
Jesper Bröring <null>
2021-02-14 16:26:04 +0000

    action build

---
+++ precache-manifest.bf933d8cee3be3581d13bcea4dbda4af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec0a694af8ff7bc69816",
+    "url": "/static/js/main.2ccee357.chunk.js"
+  },
+  {
+    "revision": "f895d4d94454e6f93215",
+    "url": "/static/js/2.a0beac84.chunk.js"
+  },
+  {
+    "revision": "ec0a694af8ff7bc69816",
+    "url": "/static/css/main.c065fbee.chunk.css"
+  },
+  {
+    "revision": "65358838da3f643717540d6e1b38883a",
+    "url": "/index.html"
+  }
+];
