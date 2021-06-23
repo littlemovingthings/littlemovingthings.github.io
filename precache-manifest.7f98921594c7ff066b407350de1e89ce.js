95c73d48aec5851084929d7d56c9ef0a5001a7ba
Jesper Bröring <jbroring@gmail.com>
2021-06-23 21:04:57 +0200

    Switching IMGIX

---
+++ precache-manifest.7f98921594c7ff066b407350de1e89ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a9d6acd60fbad3f7b42383e80f5b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cdba15b927d0f6bf60d",
+    "url": "/static/css/main.07d162e6.chunk.css"
+  },
+  {
+    "revision": "2d79f9183fa299215814",
+    "url": "/static/js/2.cfbd21a9.chunk.js"
+  },
+  {
+    "revision": "246ad901f08703825af5eeab9ed7a0eb",
+    "url": "/static/js/2.cfbd21a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdba15b927d0f6bf60d",
+    "url": "/static/js/main.a8e44278.chunk.js"
+  },
+  {
+    "revision": "0a3c3e697cc05b9ba276",
+    "url": "/static/js/runtime-main.bf1e2caf.js"
+  }
+]);
